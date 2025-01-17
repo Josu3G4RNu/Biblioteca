6247fb20f6dab9b29efa457f90fd7a81de80675d
Josué GarNu <103090680+Josu3G4RNu@users.noreply.github.com>
2025-01-16 22:44:29 -0600

    Add Query Method

--- src/main/java/com/JosueGarNu/Biblioteca/Domain/Autor/AutorRepository.java
+++ src/main/java/com/JosueGarNu/Biblioteca/Domain/Autor/AutorRepository.java
@@ -1,7 +1,13 @@
 package com.JosueGarNu.Biblioteca.Domain.Autor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
     Autor findByNombre(String nombre);
+
+    @Query("SELECT a FROM Autor a WHERE :anio BETWEEN a.anio_nacimiento AND a.anio_fallecimiento ORDER BY a.anio_nacimiento DESC")
+    List<Autor> findAutoresByAnioNacimientoBetween(int anio);
 }
--- src/main/java/com/JosueGarNu/Biblioteca/Domain/Libro/LibroRepository.java
+++ src/main/java/com/JosueGarNu/Biblioteca/Domain/Libro/LibroRepository.java
@@ -2,10 +2,15 @@ package com.JosueGarNu.Biblioteca.Domain.Libro;
 
 import com.JosueGarNu.Biblioteca.Domain.Model.Idioma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+
+    @Override
+    @Query("SELECT l FROM Libro l ORDER BY l.idioma")
+    List<Libro> findAll();
 
     boolean existsByTitulo(String titulo);
     List<Libro> findByIdiomaOrderByDescargasDesc(Idioma idioma);
